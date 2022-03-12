64cd3b0f8f19c845ee2a0dc1e8f2a3335b4d4a14
Rudresh Pandya <rudresh.pandya@Rudreshs-MacBook-Air.local>
2022-03-12 09:59:39 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      Dmsans: ["DM Sans, sans-serif"]
+    },
+    container: {
+        center: true,
+        padding: "1rem"
+      },
+  },
+  plugins: [],
+}
